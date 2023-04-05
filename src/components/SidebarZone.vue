8e07fbd582e9fc17b4a9b512b30141a0af9089e0
William Riega <williammar954@hotmail.com>
2023-04-05 11:33:18 -0500

    UPDATE: Add SidebarZone.vue y CategoryCard.vue.

--- src/components/SidebarZone.vue
+++ src/components/SidebarZone.vue
@@ -8,6 +8,7 @@ import CategoryCard from "@/components/CategoryCard.vue";
   <v-navigation-drawer app v-model = "drawer">
     <category-card title="Category 1" img-path="./assets/logo.svg" description="Full Description category"/>
     <category-card title="Category 2" img-path="./assets/logo.svg" description="Full Description category"/>
+    <category-card title="Category 1" img-path="./assets/logo.svg" description="Full Description category"/>
     <category-card title="Category 3" img-path="./assets/logo.svg" description="Full Description category"/>
   </v-navigation-drawer>
   </v-app>
