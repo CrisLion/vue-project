a08d59ccf06bfd671f11c092380156b18b616402
Cristian Quito <cristianandresquito@gmail.com>
2023-04-12 10:53:58 -0500

    header added footer added

---
+++ src/components/FooterButtons.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="sub-container">
+      <pv-button label="Save" rounded class="my-class"/>
+      <pv-button label="Cancel" rounded class="my-class"/>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "FooterButtons"
+}
+</script>
+
+<style scoped>
+
+.sub-container{
+    display: grid;
+    grid-template: "a b";
+}
+
+.my-class{
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+}
+
+</style>
