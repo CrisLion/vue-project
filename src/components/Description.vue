51e6870c41f8c11626a6f4c70f4193d4d3997618
Cristian Quito <cristianandresquito@gmail.com>
2023-04-14 11:31:14 -0500

    etiquetas areas added

--- src/components/Description.vue
+++ src/components/Description.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="sub-container">
 
-      <Label>{{$t('description')}}</Label>
-      <pv-text-area autoResize rows="5" cols="30"/>
+      <Label :for="$t('description')">{{$t('description')}}</Label>
+      <pv-text-area :id="$t('description')" autoResize rows="5" cols="30"/>
 
   </div>
 
     <div class="sub-container">
-        <pv-checkbox value="Cheese" class="my-class"/>
-        <label class="my-class">{{$t('digitalVersion')}}</label>
+        <pv-checkbox :id="$t('digitalVersion')" value="Cheese" class="my-class"/>
+        <div>
+            <label :for="$t('digitalVersion')" class="my-class">{{$t('digitalVersion')}}</label>
+        </div>
     </div>
 
 </template>
